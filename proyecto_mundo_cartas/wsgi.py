79c6f1aa6ee74f582096b0509924caf2ca73478b
Joaco32m <joaquinromero049@gmail.com>
2025-10-27 00:52:07 -0300

    feat: Add Header search and title- Mundo Cartas, Add Footer

---
+++ proyecto_mundo_cartas/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for proyecto_mundo_cartas project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyecto_mundo_cartas.settings')
+
+application = get_wsgi_application()
